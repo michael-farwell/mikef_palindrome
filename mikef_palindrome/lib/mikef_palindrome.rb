9dc32bef3cbd7a6b4f079541e8bedd68dfd1a59c
Michael Farwell <whitewolf9909@gmail.com>
2019-03-04 14:49:12 -0500

    Initialize repository

---
+++ mikef_palindrome/lib/mikef_palindrome.rb
@@ -0,0 +1,6 @@
+require "mikef_palindrome/version"
+
+module MikefPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ mikef_palindrome/lib/mikef_palindrome/version.rb
@@ -0,0 +1,3 @@
+module MikefPalindrome
+  VERSION = "0.1.0"
+end
